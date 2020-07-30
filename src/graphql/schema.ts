9f7979668f125ba5427c301b1d78c3eabd742b69
williamsk91 <williamskusnandi@gmail.com>
2020-07-30 21:18:08 +1000

    fix: production error

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -3,7 +3,10 @@ import { buildSchema } from "type-graphql";
 
 export const createSchema = async () =>
   await buildSchema({
-    resolvers: [__dirname + "/../resolvers/*.ts"],
+    resolvers: [
+      __dirname + "/../resolvers/*.ts",
+      __dirname + "/../resolvers/*.js",
+    ],
     authChecker,
-    emitSchemaFile: true
+    emitSchemaFile: process.env.NODE_ENV !== "production",
   });
