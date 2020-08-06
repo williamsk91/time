30bba8256214eff82b38c267a008af9fcbea3cba
williamsk91 <williamskusnandi@gmail.com>
2020-08-06 21:01:14 +1000

    fix: schema validation on but no validations defined

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -7,6 +7,8 @@ export const createSchema = async () =>
       __dirname + "/../resolvers/*.ts",
       __dirname + "/../resolvers/*.js",
     ],
+    // no validations set currently. Remove this line when validations are added.
+    validate: false,
     authChecker,
     emitSchemaFile: process.env.NODE_ENV !== "production",
   });
